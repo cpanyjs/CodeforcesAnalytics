100db59d76e5d8ec57f31ca062da467553da0c27
X1 Carbon <yjl9903@vip.qq.com>
2019-10-21 20:10:31 +0800

    update: report content

--- src/cli/report.ts
+++ src/cli/report.ts
@@ -10,7 +10,7 @@ let data: OutputUser[] = [];
 
 type Header = {
   name: string;
-  value: string | ((item: OutputUser) => string);
+  value: string | ((item: OutputUser) => string | number);
 };
 
 type cmpFn = (a: OutputUser, b: OutputUser) => number;
@@ -50,31 +50,95 @@ function generateMarkdownTable(headers: Header[], cmp: cmpFn) {
   );
 }
 
-function table() {
-  return generateMarkdownTable(
-    [
-      { name: '姓名', value: 'name' },
-      {
-        name: '分数',
-        value: (item) => {
-          return `<span style="color: ${rating2color(item.rating)}">${
-            item.rating
-          }</span>`;
+const tables = [
+  () =>
+    '## Rating\n\n' +
+    generateMarkdownTable(
+      [
+        { name: '姓名', value: 'name' },
+        {
+          name: 'Rating',
+          value: item =>
+            `<span style="color: ${rating2color(item.rating)}">${
+              item.rating
+            }</span>`
+        },
+        {
+          name: '场次',
+          value: item => item.contest.length
         }
+      ],
+      (a, b) => {
+        if (a.rating > b.rating) return -1;
+        else if (a.rating == b.rating) return 0;
+        else return 1;
       }
-    ],
-    (a, b) => {
-      if (a.rating > b.rating) return -1;
-      else if (a.rating == b.rating) return 0;
-      else return 1;
-    }
-  );
-}
+    ),
+  () =>
+    '## 参赛场次\n\n' +
+    generateMarkdownTable(
+      [
+        { name: '姓名', value: 'name' },
+        {
+          name: 'Rating',
+          value: item =>
+            `<span style="color: ${rating2color(item.rating)}">${
+              item.rating
+            }</span>`
+        },
+        {
+          name: '场次',
+          value: item => item.contest.length
+        }
+      ],
+      (a, b) => {
+        const la = a.contest.length,
+          lb = b.contest.length;
+        if (la > lb) return -1;
+        else if (la == lb) return 0;
+        else return 1;
+      }
+    ) +
+    '\n',
+  () =>
+    '## 总题数\n\n' +
+    generateMarkdownTable(
+      [
+        { name: '姓名', value: 'name' },
+        { name: '题数', value: item => item.solved.length }
+      ],
+      (a, b) => {
+        const la = a.solved.length,
+          lb = b.solved.length;
+        if (la > lb) return -1;
+        else if (la == lb) return 0;
+        else return 1;
+      }
+    ),
+  () =>
+    '## VP 场数\n\n' + 
+    generateMarkdownTable(
+      [
+        { name: '姓名', value: 'name' },
+        {
+          name: 'VP 场数',
+          value: item => item.vp.length
+        }
+      ],
+      (a, b) => {
+        const la = a.vp.length,
+          lb = b.vp.length;
+        if (la > lb) return -1;
+        else if (la == lb) return 0;
+        else return 1;
+      }
+    ),
+];
 
 export default function generate(info: OutputUser[]) {
   data = info;
-  const md = `# ${TITLE} Codeforces 统计
-${table()}`;
+  const md =
+    `# ${TITLE} Codeforces 统计\n\n` + tables.map(fn => fn() + '\n').join('\n');
 
   return `<!DOCTYPE html>
 <html lang="en">
@@ -83,10 +147,13 @@ ${table()}`;
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css" integrity="sha256-HbgiGHMLxHZ3kkAiixyvnaaZFNjNWLYKD/QG6PWaQPc=" crossorigin="anonymous" />
+  <style>
+  table { display: table !important; }
+  </style>
   <title>${TITLE} Codeforces 统计</title>
 </head>
-<body>
-  <div class="markdown-body">${marked(md)}</div>
+<body style="padding: 0 30px;">
+  <div style="padding: 30px 30px 100px; margin: 0 auto; width: 80%; max-width: 860px;" class="markdown-body">${marked(md)}</div>
 </body>
 </html>`;
 }
